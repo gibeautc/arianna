46e3f02c937b04ce1ce81614d8fce85304dc8e72
gibeautc <gibeautc@oregonstate.edu>
2016-12-15 14:45:52 -0800

    Combined into weather

--- alert.py
+++ alert.py
@@ -13,33 +13,5 @@ c_index=0
 city=['albany','salem','portland','newport','eugene']
 while True:
 #alerts
-	url="http://api.wunderground.com/api/803ee257021d3c0e/alerts/q/OR/"+city[c_index]+".json"
-	try:
-		response=urllib.urlopen(url)
-		data=json.loads(response.read())
-		alerts=data['alerts']
-	except:
-		print("Error Getting Data")
-		continue
-	for alert in alerts:
-		issued=alert['date']
-		description=alert['description']
-		expires=alert['expires']
-		message=alert['message']
-		print("Alerts for: "+city[c_index])
-		print(issued)
-		print("Expires: "+expires)
-		print(description)
-		print(message)
-		db_out=[str(issued),str(expires),str(description),str(message),str(1),city[c_index]]
-		try:
-			curs.execute('insert into weather_alert values(current_date(),now(),%s,%s,%s,%s,%s,%s)',db_out)
-			db.commit()
-		except:
-			print("Error: Rolling Back")
-			db.rollback()
-			print(sys.exc_info())
-	c_index+=1
-	if c_index==len(city):
-		c_index=0
+	
 	time.sleep(2000)
--- weather.py
+++ weather.py
@@ -8,26 +8,82 @@ import MySQLdb
 import sys
 db=MySQLdb.connect('localhost','auto','myvice12','main')
 curs=db.cursor()
-print("Getting Albany Weather")
 
-c_index=0
-city=['albany','salem','portland','newport','eugene']
+citys=['albany','salem','portland','newport','eugene']
 
-#current conditions
-#url="http://api.wunderground.com/api/803ee257021d3c0e/conditions/q/OR/"+city+".json"
-while True:
-#10 day forcast
-	url="http://api.wunderground.com/api/803ee257021d3c0e/forecast10day/q/OR/"+city[c_index]+".json"
+rate=0
 
-	print("Updating: "+city[c_index])
+
+def Get_current(city_name):
+	print("Getting Current: "+city_name)
+	url="http://api.wunderground.com/api/803ee257021d3c0e/conditions/q/OR/"+city_name+".json"
 	try:
+		rate+=1
 		response=urllib.urlopen(url)
 		data=json.loads(response.read())
 	except:
 		print("Error Getting Data")
-		continue
-	#print(data)
-	#pp=pprint.PrettyPrinter(indent=2)
+		return
+	cur=data['current_observation']
+	town=cur['display_location']['full']
+	temp=cur['temp_f']
+	wind=cur['wind_mph']
+	weather=cur['weather']
+	pressure=cur['pressure_in']
+	pressure_trend=cur['pressure_trend']
+	precip_today=cur['precip_today_in']
+	db_out=[city_name,str(weather),str(temp),str(wind),str(pressure),str(pressure_trend),str(precip_today)]
+	try:
+		curs.execute('insert into weather_cur values(current_date(),now(),%s,%s,%s,%s,%s,%s,%s)',db_out)
+		db.commit()
+	except:
+		print("Error: Rolling Back")
+		db.rollback()
+		print(sys.exc_info())
+
+
+def Get_alert(city_name):
+	print("Getting Alerts: "+city_name)
+	url="http://api.wunderground.com/api/803ee257021d3c0e/alerts/q/OR/"+city_name+".json"
+	try:
+		response=urllib.urlopen(url)
+		data=json.loads(response.read())
+		alerts=data['alerts']
+	except:
+		print("Error Getting Data")
+		return
+	for alert in alerts:
+		issued=alert['date']
+		description=alert['description']
+		expires=alert['expires']
+		message=alert['message']
+		#print("Alerts for: "+city_name)
+		#print(issued)
+		#print("Expires: "+expires)
+		#print(description)
+		#print(message)
+		db_out=[str(issued),str(expires),str(description),str(message),str(1),city_name]
+		try:
+			curs.execute('insert into weather_alert values(current_date(),now(),%s,%s,%s,%s,%s,%s)',db_out)
+			db.commit()
+		except:
+			print("Error: Rolling Back")
+			db.rollback()
+			print(sys.exc_info())
+
+
+
+
+def Get_forecast(city_name):
+	url="http://api.wunderground.com/api/803ee257021d3c0e/forecast10day/q/OR/"+city_name+".json"
+	print("Getting Forecast: "+city_name)
+	try:
+		rate+=1
+		response=urllib.urlopen(url)
+		data=json.loads(response.read())
+	except:
+		print("Error Getting Data")
+		return
 	forcast=data['forecast']['simpleforecast']['forecastday']
 	for day in forcast:
 		status=day['conditions']
@@ -49,7 +105,7 @@ while True:
 		#sql date format YYYY-MM-DD
 		#Will need to check if single digit day and month are right
 		db_date=str(date_year)+"-"+str(date_month)+"-"+str(date_day)
-		db_out=[db_date,str(high),str(low),str(wind_mph),wind_dir,str(wind_max),wind_mdir,str(qpf_day),str(qpf_night),str(qpf_total),str(snow_day),str(snow_night),str(snow_total),status,city[c_index]]
+		db_out=[db_date,str(high),str(low),str(wind_mph),wind_dir,str(wind_max),wind_mdir,str(qpf_day),str(qpf_night),str(qpf_total),str(snow_day),str(snow_night),str(snow_total),status,city_name]
 		try:
 			curs.execute('insert into weather_forecast values(current_date(),now(),%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',db_out)
 			db.commit()
@@ -57,7 +113,21 @@ while True:
 			print("Error: Rolling Back")
 			db.rollback()
 			print(sys.exc_info())
-	c_index+=1
-	if c_index==len(city):
-		c_index=0
-	time.sleep(600)
+
+
+
+while True:
+	if rate>10:
+		print("Starting Loop\n\n")
+		rate=0
+		for city in citys:
+			Get_forecast(city)
+			Get_alert(city)
+			delay(10)
+	time.sleep(60)
+	for city in citys:
+		Get_current(city)
+		time.sleep(10)
+	rate+=1
+		
+	
